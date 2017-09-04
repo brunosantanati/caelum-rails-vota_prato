c51a4ab812c2cf1c8362f04762f565f826a14aaf
Bruno Sant Ana <bruno.santana.ti@gmail.com>
2017-09-04 10:29:20 -0300

    Add validations

--- app/models/receita.rb
+++ app/models/receita.rb
@@ -1,5 +1,7 @@
 class Receita < ApplicationRecord
   validates_presence_of :conteudo, message: " - deve ser preenchido"
+  validates_presence_of :prato_id
+  validates_associated :prato
 
   belongs_to :prato
 end
