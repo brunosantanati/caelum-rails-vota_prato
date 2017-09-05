b4076894863afab1cc4b3e8898de9c7e648c7a19
Bruno Sant Ana <bruno.santana.ti@gmail.com>
2017-09-05 02:30:45 -0300

    Implement show restaurant

--- app/controllers/restaurantes_controller.rb
+++ app/controllers/restaurantes_controller.rb
@@ -1,5 +1,11 @@
 class RestaurantesController < ApplicationController
+  
   def index
     @restaurantes = Restaurante.order :nome
   end
+
+  def show
+    @restaurante = Restaurante.find(params[:id])
+  end
+  
 end
