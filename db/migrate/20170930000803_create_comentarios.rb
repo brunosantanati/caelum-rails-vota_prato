dc76e4445e40b401b7968937a8780b94a0fe702c
Bruno Sant Ana <bruno.santana.ti@gmail.com>
2017-09-29 21:08:52 -0300

    Generate comentario scaffold

---
+++ db/migrate/20170930000803_create_comentarios.rb
@@ -0,0 +1,11 @@
+class CreateComentarios < ActiveRecord::Migration[5.1]
+  def change
+    create_table :comentarios do |t|
+      t.text :conteudo
+      t.integer :comentavel_id
+      t.string :comentavel_type
+
+      t.timestamps
+    end
+  end
+end
